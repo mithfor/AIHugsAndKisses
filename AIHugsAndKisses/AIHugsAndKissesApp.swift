1a624d89c905c31150bff1e2a3728d18a22f68c4
Dmitriy Voronin <devoronin@gmail.com>
2025-02-14 20:20:05 +0300

    Initial Commit

---
+++ AIHugsAndKisses/AIHugsAndKissesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIHugsAndKissesApp.swift
+//  AIHugsAndKisses
+//
+//  Created by Dmitrii Voronin on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIHugsAndKissesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
